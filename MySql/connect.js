5ae855af78c45bc774a7e19e9bc2e6a285f9f540
Nitish Pal <nitishpal2013@gmail.com>
2023-07-11 01:33:22 +0530

    Reason Added

--- MySql/connect.js
+++ MySql/connect.js
@@ -1,3 +1,12 @@
+/**
+ * Reason to change the module from mysql to mysql2
+ * Latest MySql supports caching_sha2_password authentication method instead
+ * of mysql_native_password.
+ * mysql does not support the latest cryptic method to authenticate .
+ * Three Options to solve this is available here :
+ * https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+ */
+
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
